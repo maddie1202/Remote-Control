4cf87362f95371539d9a79da86d6ae93790edeca
Madeline Ferguson <lusul101@gmail.com>
2019-05-30 23:59:22 -0600

    Make compatible with Linux, Mac and Windows by switching keyboard libraries

--- remote_control.py
+++ remote_control.py
@@ -1,24 +1,40 @@
 import serial
 import json
 import re
-import keyboard
+from pynput.keyboard import Key, Controller, KeyCode
 
 def press_key(button):
-    with open ('button_config.json', 'r') as json_file:
+    with open ("button_config.json", "r") as json_file:
         json_str = json_file.read()
         configs = json.loads(json_str)
 
         try:
-            keyboard.press_and_release(configs[button])
+            with open ("virtual_key_codes.json", "r") as json_file:
+                json_str = json_file.read()
+                codes = json.loads(json_str)
+
+            keyboard = Controller()
+            keys = configs[button]
+
+            if type(keys) is list:
+                for key in keys:
+                    keyboard.press(KeyCode(codes[key]))
+                for key in keys:
+                    keyboard.release(KeyCode(codes[key]))
+            else:
+                keyboard.press(KeyCode(codes[keys]))
+                keyboard.release(KeyCode(codes[keys]))
+
+
         except KeyError:
-            print("Error,", button, "not recognized.")
+            print("Error,", button, "not configured")
 
 
-with open ('remote_frequencies.json', 'r') as json_file:
+with open ("remote_frequencies.json", "r") as json_file:
     json_str = json_file.read()
     frequencies = json.loads(json_str)
 
-arduino = serial.Serial('COM3', 115200, timeout=.1)
+arduino = serial.Serial("COM3", 115200, timeout=.1)
 last_frequency = 0
 
 while True:
@@ -41,3 +57,6 @@ while True:
             continue
 
         press_key(button)
+
+        if button == "Power" :
+            exit()
