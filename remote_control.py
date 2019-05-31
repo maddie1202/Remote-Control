0dfe050cd4f56957dbe63b4ec72b2a0be4c150d9
Madeline Ferguson <lusul101@gmail.com>
2019-05-30 22:13:08 -0600

    Add proper error handling and add hold button to button_frequncies.json

--- remote_control.py
+++ remote_control.py
@@ -8,17 +8,18 @@ def press_key(button):
         json_str = json_file.read()
         configs = json.loads(json_str)
 
-        if(button != "button not found"):
+        try:
             keyboard.press_and_release(configs[button])
-        else:
-            print("button not found")
+        except KeyError:
+            print("Error,", button, "not recognized.")
+
 
 with open ('remote_frequencies.json', 'r') as json_file:
     json_str = json_file.read()
     frequencies = json.loads(json_str)
 
 arduino = serial.Serial('COM3', 115200, timeout=.1)
-last_frequency = 65535
+last_frequency = 0
 
 while True:
     data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
@@ -27,12 +28,16 @@ while True:
         matches = re.findall("b'(\d+)'", str(data))
         frequency = matches[0]
 
-        print(frequency)
-
-        if frequency == '65535':
+        if frequency == frequencies["Hold"]:
             frequency = last_frequency
         else:
             last_frequency = frequency
 
-        button = frequencies.get(frequency, "button not found")
+        try:
+            button = frequencies[frequency]
+            print(frequencies[frequency])
+        except KeyError:
+            print("Error, ", frequency, " not recognized.")
+            continue
+
         press_key(button)
